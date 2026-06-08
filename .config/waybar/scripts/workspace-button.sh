86dfdf81e3c4c17d856dc5d9c5518f6f54f72c10
mrgustaveh <mburuantony@outlook.com>
2026-06-08 10:19:00 +0300

    minor fix

--- .config/waybar/scripts/workspace-button.sh
+++ .config/waybar/scripts/workspace-button.sh
@@ -8,8 +8,13 @@ import subprocess
 import sys
 
 workspace_id = int(sys.argv[1])
-workspaces = json.loads(subprocess.check_output(["hyprctl", "workspaces", "-j"]))
-active = json.loads(subprocess.check_output(["hyprctl", "activeworkspace", "-j"]))
+
+try:
+    workspaces = json.loads(subprocess.check_output(["hyprctl", "workspaces", "-j"], stderr=subprocess.DEVNULL))
+    active = json.loads(subprocess.check_output(["hyprctl", "activeworkspace", "-j"], stderr=subprocess.DEVNULL))
+except Exception:
+    raise SystemExit(0)
+
 match = next((ws for ws in workspaces if ws["id"] == workspace_id), None)
 
 if match is None:
